fa88216643c4ec076a8322aa8eb57a20855060e9
Vinnie <vinnie@bettordone.com>
2022-02-13 00:11:28 +0530

    put and delete endpoints

--- src/main/java/com/restapi/us/controller/CustomerController.java
+++ src/main/java/com/restapi/us/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.restapi.us.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +43,39 @@ public class CustomerController {
 			return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<Customer> getSingleCustomer(@PathVariable Long id){
+		Optional <Customer> customer = customerRepo.findById(id);
+		
+		if (customer.isPresent()) {
+			return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PutMapping("/customers/{id}")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer){
+		try {
+			return new ResponseEntity<Customer>(customerRepo.save(customer), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@DeleteMapping("/customers/{id}")
+	public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable Long id) {
+		try {
+			Optional<Customer> customer = customerRepo.findById(id);
+			if (customer.isPresent()) {
+				customerRepo.delete(customer.get());
+			}
+			return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
