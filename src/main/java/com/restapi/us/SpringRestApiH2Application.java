31e8fa5755032d91ab1f554b5d71fabb3563f76e
Vinnie <vinnie@bettordone.com>
2022-02-19 13:54:37 +0530

    Swagger changes

--- src/main/java/com/restapi/us/SpringRestApiH2Application.java
+++ src/main/java/com/restapi/us/SpringRestApiH2Application.java
@@ -2,20 +2,33 @@ package com.restapi.us;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class SpringRestApiH2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiH2Application.class, args);
 	}
-	
-	@RequestMapping(value= "/")
-	public String test() {
-		return "Home page, please type endpoint and select appropriate action to get proper response";
+
+//	@RequestMapping(value = "/")
+//	public String test() {
+//		return "Home page, please type endpoint and select appropriate action to get proper response";
+//	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.restapi.us")).build();
 	}
 
 }
