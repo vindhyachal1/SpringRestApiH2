8665fd48f393095d27bb700766319189a40b1454
Vinnie <vinnie@bettordone.com>
2022-07-15 16:15:12 +0530

    Health Check

---
+++ src/main/java/com/restapi/us/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.restapi.us.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping(value = "/")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok().build();
+    }
+}
